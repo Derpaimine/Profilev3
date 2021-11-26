3119a1f17cae0ec552a6ed8caf18f0da6c7563ec
Jordan <91262321+Derpaimine@users.noreply.github.com>
2021-11-26 16:02:58 +0200

    Add files via upload

---
+++ js/mystyle.js
@@ -0,0 +1,32 @@
+function showTestimonial(name, quote) {
+  document.getElementById("testimonialName").innerHTML = name;
+  document.getElementById("testimonialQuote").innerHTML = quote;
+}
+
+function github() {
+  window.open("https://github.com/Derpaimine");
+}
+
+function cGithub() {
+  window.open("https://github.com/Derpaimine/Calculator.git");
+}
+
+function cNetlify() {
+  window.open("https://calculatorbyjd.netlify.app");
+}
+
+function port2Github() {
+  window.open("https://github.com/Derpaimine/Profile-2.git");
+}
+
+function port2Netlify() {
+  window.open("https://gracious-mestorf-547b05.netlify.app");
+}
+
+function cardGithub() {
+  window.open("https://github.com/Derpaimine/Card-flip.git");
+}
+
+function card2Netlify() {
+  window.open("https://zealous-shirley-19d6e0.netlify.app");
+}
